35d4e8061103b8c172400febf26b119429a0f106
yeongwoo <jyy0223@naver.com>
2023-10-18 21:00:43 +0900

    [add] default layout, appbar

---
+++ lib/common/layout/default_appbar.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+import 'package:yeongwoo_kit/common/const/colors.dart';
+
+class DefaultAppBar extends StatelessWidget implements PreferredSizeWidget {
+  static const defaultAppBarHeight = 60.0;
+  final String title;
+  final List<Widget>? action;
+  final Widget? leading;
+  final double? elevation;
+
+  const DefaultAppBar({
+    Key? key,
+    required this.title,
+    this.leading,
+    this.action,
+    this.elevation = 0.3,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return AppBar(
+      centerTitle: true,
+      title: Text(
+        title,
+        style: const TextStyle(
+          fontSize: 18.0,
+          fontWeight: FontWeight.bold,
+          color: MyColor.defaultText,
+        ),
+      ),
+      leading: leading,
+      actions: action,
+      elevation: elevation,
+      foregroundColor: MyColor.defaultText,
+      backgroundColor: MyColor.scaffoldBackground,
+    );
+  }
+
+  @override
+  Size get preferredSize =>
+      const Size(double.infinity, DefaultAppBar.defaultAppBarHeight);
+}
---
+++ lib/common/layout/default_layout.dart
@@ -0,0 +1,51 @@
+import 'package:flutter/material.dart';
+import 'package:yeongwoo_kit/common/const/colors.dart';
+
+class DefaultLayout extends StatelessWidget {
+  final Widget child;
+  final bool isLoading;
+  final Widget? loadingWidget;
+  final PreferredSizeWidget? appbar;
+  final Color? backgroundColor;
+  final Widget? bottomNavigationBar;
+  final FloatingActionButton? floatingActionButton;
+
+  const DefaultLayout({
+    required this.child,
+    this.isLoading = false,
+    this.loadingWidget,
+    this.appbar,
+    this.backgroundColor,
+    this.bottomNavigationBar,
+    this.floatingActionButton,
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Stack(
+      children: [
+        Scaffold(
+          appBar: appbar,
+          backgroundColor: backgroundColor ?? MyColor.scaffoldBackground,
+          body: SafeArea(
+            child: GestureDetector(
+              onTap: () {
+                FocusScope.of(context).unfocus();
+              },
+              child: child,
+            ),
+          ),
+          bottomNavigationBar: bottomNavigationBar,
+          floatingActionButton: floatingActionButton,
+        ),
+        Positioned.fill(
+          child: Visibility(
+            visible: isLoading,
+            child: loadingWidget ?? const CircularProgressIndicator(),
+          ),
+        )
+      ],
+    );
+  }
+}
