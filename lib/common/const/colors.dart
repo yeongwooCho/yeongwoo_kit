bbba7c3d492d68f0be0bbfbf97b4f8c787c0480e
yeongwoo <jyy0223@naver.com>
2023-10-18 19:46:40 +0900

    [add] 기본 컬러 셋팅

---
+++ lib/common/const/colors.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+
+class MyColor {
+  // Main
+  static const primary = Color(0xFF4273B1);
+  static const secondary = Color(0xFF081F5C);
+
+  // Text Color
+  static const defaultText = Color(0xFF1C1C1E);
+  static const whiteText = Color(0xFFFFFFFF);
+
+  // Error
+  static const error = Color(0xFFFF555F);
+
+  // Background Color
+  static const barrier = Color(0X661C1C1E);
+  static const empty = Color(0X00000000);
+  static const background = Color(0xFFFFFFFF);
+
+  // Grey
+  static const darkGrey = Color(0xFF888888);
+  static const middleGrey = Color(0xFFDCDCDD);
+  static const lightGrey = Color(0xFFEFF1F3);
+
+  // Text Field Color
+  static const inputBorder = Color(0xFFAEAEB2);
+  static const placeHolder = Color(0XFF8E8E93);
+
+  // Stroke Color
+  static const strokeTextField = Color(0XFFAEAEB2);
+  static const shadow = Color(0XFF000000);
+
+  // disabled Button Color
+  static const disabledButtonForeground = Color(0xFF999898);
+  static const disabledButtonBackground = Color(0xFFDCDCDC);
+
+  // KAKAO LOGIN
+  static const kakaoForeground = Color(0xD9000000);
+  static const kakaoBackground = Color(0xFFFEE500);
+
+  // APPLE LOGIN
+  static const appleForeground = Color(0xFFFFFFFF);
+  static const appleBackground = Color(0xFF000000);
+}
+
