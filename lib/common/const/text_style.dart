75d2a1d17b7e7f2e8ae42cdcbbbd4c15145478c5
yeongwoo <jyy0223@naver.com>
2023-10-18 19:53:30 +0900

    [add] text style 지정

---
+++ lib/common/const/text_style.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+
+class MyTextStyle {
+  static const appName = TextStyle(
+    fontSize: 36.0,
+    fontWeight: FontWeight.bold,
+  );
+
+  static const headTitle = TextStyle(
+    fontSize: 24.0,
+    fontWeight: FontWeight.bold,
+  );
+
+  static const bodyTitleBold = TextStyle(
+    fontSize: 20.0,
+    fontWeight: FontWeight.bold,
+  );
+
+  static const bodyBold = TextStyle(
+    fontSize: 16.0,
+    fontWeight: FontWeight.bold,
+  );
+
+  static const bodyMedium = TextStyle(
+    fontSize: 16.0,
+    fontWeight: FontWeight.w500,
+  );
+
+  static const bodyRegular = TextStyle(
+    fontSize: 16.0,
+    fontWeight: FontWeight.w300,
+  );
+
+  static const description = TextStyle(
+    fontSize: 14.0,
+    fontWeight: FontWeight.w400,
+  );
+}
