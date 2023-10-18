af050cb0d40e6f72f54bbae7e907c6e01a6f238d
yeongwoo <jyy0223@naver.com>
2023-10-18 21:28:03 +0900

    [add] default button

---
+++ lib/common/layout/default_button.dart
@@ -0,0 +1,50 @@
+import 'package:flutter/material.dart';
+import 'package:yeongwoo_kit/common/const/colors.dart';
+import 'package:yeongwoo_kit/common/const/text_style.dart';
+
+class DefaultElevatedButton extends StatelessWidget {
+  final Widget child;
+  final VoidCallback onPressed;
+  final Color foregroundColor;
+  final Color backgroundColor;
+  final TextStyle? textStyle;
+  final double? elevation;
+  final Size? minimumSize;
+  final OutlinedBorder? shape;
+  final bool isBorderSide;
+
+  const DefaultElevatedButton({
+    super.key,
+    required this.child,
+    required this.onPressed,
+    this.foregroundColor = MyColor.whiteText,
+    this.backgroundColor = MyColor.primary,
+    this.textStyle = MyTextStyle.bodyBold,
+    this.elevation = 0.0,
+    this.minimumSize = const Size(100, 50),
+    this.shape,
+    this.isBorderSide = false,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return ElevatedButton(
+      style: ElevatedButton.styleFrom(
+        foregroundColor: foregroundColor,
+        backgroundColor: backgroundColor,
+        textStyle: textStyle,
+        elevation: elevation,
+        minimumSize: minimumSize,
+        shape: shape ??
+            RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(8.0),
+              side: isBorderSide
+                  ? BorderSide(width: 2.0, color: foregroundColor)
+                  : BorderSide.none,
+            ),
+      ),
+      onPressed: onPressed,
+      child: child,
+    );
+  }
+}
