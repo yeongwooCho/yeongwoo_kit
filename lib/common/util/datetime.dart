791fcc634640490426b87b2b6e92c35e478e95fb
yeongwoo <jyy0223@naver.com>
2023-10-20 18:00:08 +0900

    [add] extension 잘못 설정된 것 수정

--- lib/common/util/datetime.dart
+++ lib/common/util/datetime.dart
@@ -1,19 +1,9 @@
 extension DateString on DateTime {
-  String convertDateTimeToDateString({
-    required DateTime datetime,
-  }) {
-    return '${datetime.year}-${datetime.month.toString().padLeft(2, '0')}-${datetime.day.toString().padLeft(2, '0')}';
+  String convertDateTimeToDateString() {
+    return '$year-${month.toString().padLeft(2, '0')}-${day.toString().padLeft(2, '0')}';
   }
 
-  String convertDateTimeToHour({
-    required DateTime datetime,
-  }) {
-    return '${datetime.year}-${datetime.month.toString().padLeft(2, '0')}-${datetime.day.toString().padLeft(2, '0')} ${datetime.hour.toString().padLeft(2, '0')}';
-  }
-
-  String convertDateTimeToMinute({
-    required DateTime datetime,
-  }) {
-    return '${datetime.year}-${datetime.month.toString().padLeft(2, '0')}-${datetime.day.toString().padLeft(2, '0')} ${datetime.hour.toString().padLeft(2, '0')}:${datetime.minute.toString().padLeft(2, '0')}';
+  String convertDateTimeToMinute() {
+    return '$year-${month.toString().padLeft(2, '0')}-${day.toString().padLeft(2, '0')} ${hour.toString().padLeft(2, '0')}:${minute.toString().padLeft(2, '0')}';
   }
 }
