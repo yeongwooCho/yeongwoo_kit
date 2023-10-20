453ac895fb067e38ccf9499ff440a80b7017ab63
yeongwoo <jyy0223@naver.com>
2023-10-20 18:32:53 +0900

    [add] money format extension 잘못된 지정 수정

--- lib/common/util/money_format.dart
+++ lib/common/util/money_format.dart
@@ -1,13 +1,11 @@
 import 'package:intl/intl.dart';
 
-extension MoneyString on String {
+extension MoneyString on int {
   // 이메일 포맷 검증
-  String convertIntToMoneyString({
-    required int number,
-  }) {
+  String convertIntToMoneyString() {
     final NumberFormat numberFormat = NumberFormat('###,###,###,###');
     // print(numberFormat.format(1000000);
     // # ==> 1,000,000
-    return numberFormat.format(number);
+    return numberFormat.format(this);
   }
 }
