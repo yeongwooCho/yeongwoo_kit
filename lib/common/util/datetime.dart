a2492562322e858514b075fe72bf2539218cfb74
yeongwoo <jyy0223@naver.com>
2023-10-24 11:34:20 +0900

    [add] 특정 날짜가 올해 몇번째 주인지 반환하는 함수 DateTime extension에 추가

--- lib/common/util/datetime.dart
+++ lib/common/util/datetime.dart
@@ -1,9 +1,22 @@
 extension DateString on DateTime {
   String convertDateTimeToDateString() {
+    // 2023-12-25
     return '$year-${month.toString().padLeft(2, '0')}-${day.toString().padLeft(2, '0')}';
   }
 
   String convertDateTimeToMinute() {
+    // 2023-12-25 23:07
     return '$year-${month.toString().padLeft(2, '0')}-${day.toString().padLeft(2, '0')} ${hour.toString().padLeft(2, '0')}:${minute.toString().padLeft(2, '0')}';
+  }
+}
+
+extension WeekNumber on DateTime {
+  // this 가 올해의 몇 번째 주인지 반환하는 코드
+  int getWeekNumber() {
+    DateTime firstDayOfYear = DateTime(year, 1, 1);
+    int daysOffset = (DateTime.thursday - firstDayOfYear.weekday) % 7;
+    DateTime firstThursday = firstDayOfYear.add(Duration(days: daysOffset));
+    int weekNumber = 1 + ((difference(firstThursday).inDays) / 7).floor();
+    return weekNumber;
   }
 }
